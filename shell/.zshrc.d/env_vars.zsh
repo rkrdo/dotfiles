24aa198a7973be397ba263ee107732e901a24c68
Ricardo Cruz <ricardocruz.a89@gmail.com>
2020-08-22 16:44:53 -0500

    Initial commit

---
+++ shell/.zshrc.d/env_vars.zsh
@@ -0,0 +1,6 @@
+export EDITOR=vim
+export VIMCONFIG=~/.vim
+export VIMDATA=~/.vim
+export NNN_USE_EDITOR=1
+# use ripgrep with fzf
+export FZF_DEFAULT_COMMAND='rg --files --follow --hidden --no-ignore-vcs'
