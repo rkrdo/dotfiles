20bbcf1d9f9bfa93a6b19f70c3d4784d193aa20d
Ricardo Cruz <ricardocruz.a89@gmail.com>
2021-07-15 23:13:02 -0500

    Make dotfiles compatible with m1 Mac
    - Remove asdf initialization (use the oh-my-zsh plugin instead)
    - Use the asdf-java mac integration
    - Added homebrew script for apple silicon installations

--- shell/dot-zshrc.d/asdf.zsh
+++
@@ -1,7 +0,0 @@
-# asdf #
-# https://asdf-vm.com/#/core-manage-asdf-vm?id=add-to-your-shell
-# from brew --prefix asdf
-. /usr/local/opt/asdf/asdf.sh
-
-# asdf-java
-. ~/.asdf/plugins/java/set-java-home.zsh
---
+++ shell/dot-zshrc.d/homebrew.zsh
@@ -0,0 +1,2 @@
+# https://github.com/Homebrew/discussions/discussions/446#discussioncomment-263078
+eval $(/opt/homebrew/bin/brew shellenv)
