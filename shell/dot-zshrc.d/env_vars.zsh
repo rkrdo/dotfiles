bab5b73b31399afc7a5d1505e6487b2f6ec55dcd
Ricardo Cruz <ricardocruz.a89@gmail.com>
2022-03-10 21:13:13 -0600

    update config

--- shell/dot-zshrc.d/env_vars.zsh
+++ shell/dot-zshrc.d/env_vars.zsh
@@ -4,3 +4,5 @@ export NNN_USE_EDITOR=1
 # export FZF_DEFAULT_COMMAND='rg --files --follow --hidden --no-ignore-vcs'
 # https://github.com/junegunn/fzf#respecting-gitignore
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --follow --exclude .git'
+export FZF_TMUX=1
+export FZF_TMUX_OPTS="-p"
