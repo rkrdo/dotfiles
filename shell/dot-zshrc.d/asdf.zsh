b07e55d0a6444f9327385c7e9b687ad54f1f2701
Ricardo Cruz <ricardocruz.a89@gmail.com>
2022-06-17 23:15:16 -0500

    Add asdf-direnv

--- shell/dot-zshrc.d/asdf.zsh
+++ shell/dot-zshrc.d/asdf.zsh
@@ -5,3 +5,6 @@
 
 # asdf-java
 [ -f $HOME/.asdf/plugins/java/set-java-home.zsh ] && . ~/.asdf/plugins/java/set-java-home.zsh
+
+# asdf-direnv
+[ -f $HOME/.config/asdf-direnv/zshrc ] && source ${XDG_CONFIG_HOME:-$HOME/.config}/asdf-direnv/zshrc
