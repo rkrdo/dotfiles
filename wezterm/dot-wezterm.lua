bac759e9c0b55b992f66ce4a3a4bb3c475f639c2
Ricardo Cruz <ricardocruz.a89@gmail.com>
2022-10-19 22:27:34 -0500

    Wezterm config

---
+++ wezterm/dot-wezterm.lua
@@ -0,0 +1,8 @@
+local wezterm = require 'wezterm'
+return {
+  font = wezterm.font 'MesloLGS Nerd Font',
+  font_size = 13.0,
+  color_scheme = "palenight (Gogh)",
+  hide_tab_bar_if_only_one_tab = true,
+  window_decorations = "RESIZE"
+}
